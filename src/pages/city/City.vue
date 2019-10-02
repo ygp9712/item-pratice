28958f303fde3269e4def268061a279f5dd3afd9
Yang <1175430716@qq.com>
2019-10-02 14:55:50 +0800

    实现城市搜索栏

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,19 +1,22 @@
 <template>
  <div>
     <city-header></city-header>
+    <city-Search></city-Search>
  </div>
 </template>
 
 <script>
     import CityHeader from './components/header';
+    import CitySearch from './components/search';
     export default {
       name: 'City',
       components: {
         CityHeader: CityHeader,
+        CitySearch: CitySearch,
       }
     }
 </script>
 
 <style lang="stylus" scoped>
-  
+
 </style>
