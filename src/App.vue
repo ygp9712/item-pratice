515ee988cf9cfc6f253840a44acefe0270bf9f12
Yang <1175430716@qq.com>
2019-10-05 21:00:52 +0800

    keepalive和activated的使用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
     <!-- routerview显示当前路由地址所对应的内容 -->
+    <!-- keepalive的作用是页面一旦渲染就保存在内存当中，就算页面跳转也不会重新请求数据 -->
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -20,7 +20,7 @@ import HomeIcons  from './components/icons';
 import HomeRecommend from './components/Recommend';
 import HomeWeekend from './components/weekend';
 import axios from 'axios';
-
+import { mapState } from 'vuex';
 export default {
   name:'Home',
   components:{
@@ -31,15 +31,20 @@ export default {
     HomeWeekend: HomeWeekend,
   },data () {
       return {
+        lastCity: '',
+        /* 用于记录最后一次首页上城市的名称 */
         swiperList: [],
         iconList: [],
         recommendList: [],
         weekendList: [],
       }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods:{
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -56,8 +61,18 @@ export default {
   },
   mounted () {
     /* 页面一旦挂载就调用函数 */
-    this.getHomeInfo()
+    this.lastCity = this.city;
+    this.getHomeInfo();
+    /* 问题：一旦页面通过路由跳转回来就会重新渲染页面，
+    造成重新请求json文件的问题，会影响效率 */
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.getHomeInfo();
+      this.lastCity = this.city;
+    }
   }
+  /* 使用keepalive后页面第一次渲染会触发mounted和activated，之后都只会触发activated */
 }
 </script>
 
